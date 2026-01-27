e7ccd1deecfc20cb09ece0638269445872430993
Joseph DiMaggio <jcdimagg@amazon.com>
2026-01-27 00:44:46 +0000

    enhancements to schenario judger, added ProcessData

--- compliance_utils.py
+++ compliance_utils.py
@@ -154,3 +154,22 @@ CALCULATOR_TOOL = {
         }
     }
 }
+
+####################
+def print_color(text, color='white'):
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m', 
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'magenta': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m'
+    }
+    reset = '\033[0m'
+    print(f"{colors.get(color, colors['white'])}{text}{reset}")
+
+# Usage:
+# print_color("Error message", 'red')
+# print_color("Success message", 'green')
+# print_color("Warning message", 'yellow')
